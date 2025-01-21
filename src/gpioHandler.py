35115bd0c925518c06f58ac6e52140c95429f810
Reem <reem.r.khater@gmail.com>
2025-01-21 16:12:56 +0100

    gestion anti-rebond dans soundPlayer

--- src/gpioHandler.py
+++ src/gpioHandler.py
@@ -2,18 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 # Liste des broches GPIO associées aux boutons
-BUTTON_PINS = [17, 18, 27, 22, 23, 24]  # À adapter selon ton câblage
+BUTTON_PINS = [7,8,9,12,13,14]  
+DEBOUNCE_TIME = 0.2 #delai apres la detection d'une pression pour ignorer les signaux de rebond
 
 def setup_gpio():
-    """Initialise les broches GPIO."""
+    # Initialisation des broches GPIO.
     GPIO.setmode(GPIO.BCM)  # Mode BCM pour numéroter les broches
     for pin in BUTTON_PINS:
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Résistance de pull-down
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Résistance pull-down
 
 def wait_for_button_press():
-    """Attend qu'un des boutons soit pressé et retourne le numéro de broche."""
+    # Attend qu'un des boutons soit pressé et retourne le numéro de broche
     while True:
         for pin in BUTTON_PINS:
             if GPIO.input(pin) == GPIO.HIGH:  # Bouton pressé
-                return pin
-        time.sleep(0.01)  # Petit délai pour éviter une boucle trop rapide
+                time.sleep(DEBOUNCE_TIME) # anti-rebond
+                if GPIO.input(pin) == GPIO.HIGH: # verification apres stabilisation
+                    return pin
+        time.sleep(0.01)  # délai pour éviter une boucle trop rapide
